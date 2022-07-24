4e9b31db522410d064112133c17655ba0a98b884
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-07-24 12:38:59 +0200

    change finish draw pos

--- src/draw/draw_finish_game.c
+++ src/draw/draw_finish_game.c
@@ -6,7 +6,7 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 15:56:58 by yrabby            #+#    #+#             */
-/*   Updated: 2022/07/24 11:51:59 by yrabby           ###   ########.fr       */
+/*   Updated: 2022/07/24 12:38:23 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ void	draw_finish_game(t_meta *m)
 {
 	mlx_string_put(meta_get_mlx(m), \
 					meta_get_win(m), \
-					END_MSG_I * IMG_SIZE, \
 					0, \
+					25, \
 					MOVE_COUNTER_COLOR, \
-					"Done! press 'esc' to exit :)");
+					"Done!");
 }
